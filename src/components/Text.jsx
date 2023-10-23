c4d1974cc907ad518baf74e778b9547440c97985
Usama Rahman <rahmanusama390@gmail.com>
2023-10-23 20:42:16 +0600

    !

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -18,11 +18,29 @@ function Text() {
 
   const [text, setText] = useState("")
 
+  const [dark ,setDark] = useState({
+    color: "black",
+    backgroundColor : "white"
+  })
+
+  let toggledark = () => {
+    if (dark.color == 'white') {
+      setDark({
+        color: "white",
+        backgroundColor : "black"
+      })   
+      }else{
+        setDark({
+          color: "black",
+          backgroundColor : "white"
+        })
+    }
+  }
   let wordCount = text.split(' ').length
 
   return (
     <>
-      <div className="mt-8">
+      <div className="mt-8" style={dark}>
         <label htmlFor="message" className="block mb-3 pt-3 text-lg font-medium text-gray-600 text-center">Your message</label>
 
         <textarea 
@@ -50,6 +68,18 @@ function Text() {
            Convert to LowerCase
         </button>
 
+{/* Toggle Buttton Start */}
+
+         <button onClick={toggledark}>
+          <label className="relative inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer" />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-600">Toggle me</span>
+            </label>
+         </button>
+
+{/* Toggle Buttton End */}
+
         <div className='text-gray-600'>
           <h2 className='text-lg font-medium'>Your Text summary</h2>
           <p>{wordCount} words and {text.length} caracters</p>
@@ -57,6 +87,7 @@ function Text() {
           <h2 className='text-lg font-medium'>Preview</h2>
           <p className='bg-gray-200 px-4 py-3 rounded-md'>{text}</p>
         </div>
+       
       </div>
     </>
   )
