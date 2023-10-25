d4ddbb7bafecc901ba5c0108a60de33fb0ce99b8
Usama Rahman <rahmanusama390@gmail.com>
2023-10-25 21:15:09 +0600

    Q

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,8 +2,8 @@ export default function Blog() {
   return (
     <div>
 
-<main className="pt-8 pb-16 lg:pt-16 lg:pb-24 text-white bg-gray-900 antialiased">
-  <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+<main className="pt-8 pb-16 mt-10 lg:pt-16 lg:pb-24 text-white bg-gray-900 antialiased">
+  <div className="flex justify-between mt-4 px-4 mx-auto max-w-screen-xl ">
       <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
           <header className="mb-4 lg:mb-6 not-format">
               <address className="flex items-center mb-6 not-italic">
